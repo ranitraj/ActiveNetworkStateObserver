2f53aac8a80aa0862b6dd1fc5a68b09ff1f53113
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2021-10-10 02:18:06 +0530

    Implementing layout for MainActivity

--- app/src/main/java/com/android/ranit/activenetworkstatemonitor/MainActivity.java
+++ app/src/main/java/com/android/ranit/activenetworkstatemonitor/MainActivity.java
@@ -1,15 +1,20 @@
 package com.android.ranit.activenetworkstatemonitor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+
+import com.android.ranit.activenetworkstatemonitor.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    private ActivityMainBinding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
